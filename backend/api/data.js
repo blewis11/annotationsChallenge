fab886f7c679cda7982c969a8f6286faf401e18e
blewis11 <bejallewis@hotmail.com>
2019-10-25 00:31:48 +0200

    move annotation data into its own separate file

---
+++ backend/api/data.js
@@ -0,0 +1,36 @@
+const words =  [ 
+  {
+      id: 1,
+      type: 'word',
+      value: 'Sicherheit',
+      definition: 1
+  },
+  {
+      id: 2,
+      type: 'word',
+      value: 'Kombinationstherapie',
+      definition: 2
+  },
+]
+
+const definitions = [
+  {
+      id: 1,
+      type: 'definition',
+      value: `Im Kontext von klinischen Studien bezeichnet der Begriff die Schwere, Häufigkeit und Art
+      von unerwünschten Nebenwirkungen, die unter der Behandlung mit einem Medikament auftreten`,
+  },
+  {
+      id: 2,
+      type: 'definition',
+      value: `Anwendung von zwei oder mehr Therapien zur Behandlung der gleichen Erkrankung. Es
+      kann dabei um die Behandlung mit mehreren Medikamenten oder auch um eine Anwendung mehrerer
+      Therapieformen (z.B. Medikamente plus Bestrahlung) gehen. Nicht damit gemeint ist die Einnahme von
+      mehreren Medikamenten zur Behandlung verschiedener Erkrankungen`,
+  }
+]
+
+export {
+  words,
+  definitions
+}
